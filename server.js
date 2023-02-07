d8a73736324bcb5a992e20cc8f02f4a14080004f
Mat Jackson <43732109+maja5627@users.noreply.github.com>
2023-02-07 09:26:59 -0700

    allow mongo connection sting to be overriden

--- server.js
+++ server.js
@@ -6,7 +6,14 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 app.use(require("./services/records.controller"));
+const config = require ('./config.json');
+
  
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
 });
+
+if(proccess.env.MONGOCONNECTION) {
+  console.log('got override for monogo connection sring');
+  config.connectionString = proccess.env.MONGOCONNECTION;
+}
