a50f0d938916af69efecef6629696ee39993397f
Mat Jackson <43732109+maja5627@users.noreply.github.com>
2023-02-08 11:10:51 -0700

    remove

--- server.js
+++ server.js
@@ -8,9 +8,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(__dirname));
 
-app.get("/*", function(req, res) {
-  res.sendFile(path.join(__dirname, "index.html"));
-});
 app.use(require("./services/records.controller"));
 const config = require ('./config.json');
 
