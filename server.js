b863d91741925a54893d37eb28489073c3c68360
Mat Jackson <43732109+maja5627@users.noreply.github.com>
2023-02-07 18:43:28 -0700

    You need to enable JavaScript to run this app.

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(express.static(__dirname));
+
+app.get("/*", function(req, res) {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
 app.use(require("./services/records.controller"));
 const config = require ('./config.json');
 
