97140189de88e94570dfdecc40fdbd30b515bd71
Mat Jackson <43732109+maja5627@users.noreply.github.com>
2023-02-07 18:19:32 -0700

    urlencode

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ require("dotenv").config({ path: "./config.env" });
 const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(require("./services/records.controller"));
 const config = require ('./config.json');
 
